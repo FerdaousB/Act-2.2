9d357b40968f66583738fd90ad3f75770e843876
fboussemma <fboussemma@TALAN-N46L61D.dom.tti>
2021-10-21 11:20:26 +0100

    first commit

---
+++ Act-2.2/src/act221/Main.java
@@ -0,0 +1,33 @@
+package act221;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int Nb;
+		Scanner sc = new Scanner(System.in);
+        System.out.println(" Saisir un nombre entre 10 et 30 inclus : ");
+		Nb =sc.nextInt();
+		
+		 try {
+			 
+	           
+			 if (Nb < 30 || Nb > 10) {
+			        throw new Exception(" The value is not in the allowed interval");
+			        
+			 }
+			    
+	           
+	        } catch (Exception e) {
+	        	
+	        	System.out.println(e.getMessage());
+	            e.printStackTrace();
+	        } 
+	    }
+        
+	}
+
+
