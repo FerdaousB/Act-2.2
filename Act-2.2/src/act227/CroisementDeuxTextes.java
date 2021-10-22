0b612e1db874e339185fe6fe40a6be13b4179a5c
FerdaousB <91906989+FerdaousB@users.noreply.github.com>
2021-10-22 08:36:00 +0100

    Update CroisementDeuxTextes.java

--- Act-2.2/src/act227/CroisementDeuxTextes.java
+++ Act-2.2/src/act227/CroisementDeuxTextes.java
@@ -11,47 +11,43 @@ import java.util.ArrayList;
 public class CroisementDeuxTextes {
 
 	public static void main(String[] args) throws IOException {
-		
+		 
+		 String line ;
 		 BufferedReader br = new BufferedReader(new FileReader("src/act227/Texte1.txt"));  
 		 BufferedReader br1 = new BufferedReader(new FileReader("src/act227/Texte2.txt"));       
-
-	     StringBuffer sb = new StringBuffer();    
-         String line ;
+	         StringBuffer sb = new StringBuffer();    
 		 ArrayList<String>  lignes1= new ArrayList<String> () ;
 		 ArrayList<String>  lignes2= new ArrayList<String> () ;
 		 ArrayList<String>  lignes3= new ArrayList<String> () ;
 
-		     while((line = br.readLine()) != null)
-	     {
-	        sb.append(line);      
-	        sb.append("\n");    
-	        lignes1.add(line);
-
-	        
-	     }
+		 while((line = br.readLine()) != null)
+	         {
+	              sb.append(line);      
+	              sb.append("\n");    
+	              lignes1.add(line);
+                 }
 		     
-		     while((line = br1.readLine()) != null)
-		     {
-		        sb.append(line);      
-		        sb.append("\n");    
-		        lignes2.add(line);
+		 while((line = br1.readLine()) != null)
+		 {
+		      sb.append(line);      
+		      sb.append("\n");    
+		      lignes2.add(line);
 
-		     }
-	     System.out.println(sb.toString());
+		 }
+	         System.out.println(sb.toString());
 		
-	     PrintWriter writer = new PrintWriter(new FileWriter("src/act227/nouveauTexte.txt"));
-    	 int i =0;
+	         PrintWriter writer = new PrintWriter(new FileWriter("src/act227/nouveauTexte.txt"));
+    	         int i =0;
     	 
-    	 while ( i < lignes1.size() ) {
-    		 while ( i < lignes2.size() ) {
+    	         while ( i < lignes1.size() ) {
+    		    while ( i < lignes2.size() ) {
     			 
     			 lignes3.add(lignes1.get(i));
-    		     lignes3.add(lignes2.get(i));
-    	 } 
-	      }
-    	 
-    	
-    	      writer.println(lignes3);
+    		            lignes3.add(lignes2.get(i));
+    	         } 
+	         }
+    	         
+		 writer.println(lignes3);
 		      
     	      
 		    	
